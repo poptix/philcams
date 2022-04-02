02079f7b3f8702f50d279d6dc85c6b77b69c0778
Matt Hallacy <github@poptix.net>
2022-04-02 03:10:32 +0000

    It's more resilient if we bind mount a config file

--- philcams.sh
+++ philcams.sh
@@ -1,33 +1,11 @@
 #! /bin/bash
 
-# List of cameras. These URLs work for Dahua and similar cameras, a good way to discover the proper URL is by browsing with ONVIF Device Manager on desktop, or Onvier on Android. 
-# Don't forget to enter your username/password, which is sometimes different for Onvif 
-CAMS="
-rtsp://admin:admin@192.168.1.14:554/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif
-rtsp://admin:admin@192.168.1.15:554/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif
-"
-
 # Port you want to listen on. If this host isn't publicly accessible you'll need to port forward. 
 # If you change this, you need to update the port in htdocs/sample.html
 PORT=8181
 
 # Start docker. 
 docker pull roverr/rtsp-stream:2 2>&1 > /dev/null
-docker kill philcams 2>&1 > /dev/null
-docker rm philcams 2>&1 > /dev/null
 echo Starting Docker on listening port $PORT
-docker run -d --name philcams --restart unless-stopped -p $PORT:8080 roverr/rtsp-stream:2
+docker run -d -e 'RTSP_STREAM_AUDIO_ENABLED=true' -e 'RTSP_STREAM_CORS_ENABLED=true' -e 'RTSP_STREAM_CORS_ALLOWED_ORIGIN=*' -v $PWD/rtsp-stream.yml:/app/rtsp-stream.yml --restart unless-stopped -p $PORT:8080 poptix/rtsp-stream:2
 
-# Add the cameras to docker
-COUNT=0
-echo Starting Streams
-for i in $CAMS; do 
-	RESULT=`curl -s http://127.0.0.1:$PORT/start   -H 'Content-Type: application/json;charset=UTF-8' --data-raw "{\"uri\":\"$i\",\"alias\":\"$COUNT\"}"`
-	if [ `echo ${RESULT} |grep -c '"running":true'` -le 0 ]; then
-		echo Stream number $COUNT failed for some reason, usually a bad password.
-		echo $RESULT
-	else 
-		echo Stream $COUNT started successfully.
-	fi
-	COUNT=$(expr $COUNT + 1)
-done
